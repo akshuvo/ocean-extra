5be8a904640e2c303d9f2fe7d9cf8e067dcabd75
web-devise <manager@devise.com.ua>
2022-02-15 11:25:01 +0200

    Wizard Fatal Error on Importing Fix

--- includes/wizard/wizard.php
+++ includes/wizard/wizard.php
@@ -714,13 +714,17 @@ if (!class_exists('Ocean_Extra_Theme_Wizard')):
             if (isset($_POST['ocean-retina-logo']))
                 set_theme_mod('ocean_retina_logo', $_POST['ocean-retina-logo']);
 
-            if (isset($_POST['ocean-site-title']))
-                $site_title = sanitize_hex_color( $_POST['ocean-site-title'] );
-                update_option('blogname', esc_html($site_title));
+            if ( isset( $_POST['ocean-site-title'] ) ) {
+                $site_title = wp_filter_nohtml_kses( $_POST['ocean-site-title'] );
+            }
+            $this->removeElementorUpdateOptionAction( 'blogname' );
+            update_option( 'blogname', esc_html( $site_title ) );
 
-            if (isset($_POST['ocean-tagline']))
+            if ( isset( $_POST['ocean-tagline'] ) ) {
                 $site_tagline = wp_filter_nohtml_kses( $_POST['ocean-tagline'] );
-                update_option('blogdescription', esc_html($site_tagline));
+            }
+            $this->removeElementorUpdateOptionAction( 'blogdescription' );
+            update_option( 'blogdescription', esc_html( $site_tagline ) );
 
             if (isset($_POST['ocean-favicon']))
                 update_option('site_icon', $_POST['ocean-favicon']);
@@ -853,7 +857,21 @@ if (!class_exists('Ocean_Extra_Theme_Wizard')):
             wp_clear_scheduled_hook('add_second_notice');
 
         }
-
+        
+        /**
+         * Remove Elementor Update Option
+         */
+        private function removeElementorUpdateOptionAction( $option_name ) {
+            $action_name = 'update_option_' . $option_name;
+            global $wp_filter;
+            if ( isset( $wp_filter[ $action_name ] ) ) {
+                foreach ( $wp_filter[ $action_name ]->callbacks as $priority => $callbacks ) {
+                    foreach ( $callbacks as $callback_key => $callback_data ) {
+                        unset ( $wp_filter[ $action_name ]->callbacks[ $priority ][ $callback_key ] );
+                    }
+                }
+            }
+        }
     }
 
     new Ocean_Extra_Theme_Wizard();
